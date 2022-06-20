1df426e630acb175366e06f507b5ab8e2a5ed3d6
GiovaniiDias <giovani.eb3@gmail.com>
2022-06-20 19:06:54 -0300

    pastas do projeto

---
+++ projeto-kotlin/FinancasK/app/src/main/java/br/com/alura/financask/ListaTransacoesActivity.kt
@@ -0,0 +1,12 @@
+package br.com.alura.financask
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ListaTransacoesActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)  {
+        super.onCreate(savedInstanceState)
+        super.setContentView(R.layout.activity_lista_transacoes)
+    }
+}
